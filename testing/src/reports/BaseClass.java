02f40caf3817cbdff65e39792df2f6a8a72a51b5
vporrasg <53625582+vporrasg@users.noreply.github.com>
2020-06-02 20:08:47 -0600

    .

--- testing/src/reports/BaseClass.java
+++ testing/src/reports/BaseClass.java
@@ -1,12 +1,25 @@
 package reports;
 
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BaseClass {
     static WebDriver driver;
     static String chromePath = System.getProperty("user.dir" + "\\drivers\\chromedriver.exe");
 
     public static WebDriver getDriver(){
+        if (driver == null){
+            System.setProperty("webdriver.chrome.driver", chromePath);
+            driver = new ChromeDriver();
+            driver.manage().window().maximize();
+        }
+        return driver;
+    }
+
+    public static void takeScreenShot (WebDriver, String fileWitchPath){
+        TakesScreenshot scrShot
 
     }
+
 }
