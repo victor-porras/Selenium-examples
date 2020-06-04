bf01919a5e9eb5a8d57e9430d3158d7edbc187a9
vporrasg <53625582+vporrasg@users.noreply.github.com>
2020-06-04 02:22:02 -0600

    Intermedium final

--- testing/src/video/VideoRecorder.java
+++ testing/src/video/VideoRecorder.java
@@ -3,6 +3,11 @@ package video;
 import org.monte.media.Format;
 import org.monte.media.math.Rational;
 import org.monte.screenrecorder.ScreenRecorder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 import java.awt.*;
 import java.io.File;
@@ -29,14 +34,37 @@ public class VideoRecorder {
 
         GraphicsConfiguration gc =GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().getDefaultConfiguration();
 
-        this.screenRecorder = new SpecializedScreenRecorder(gc,captureSize,
+        this.screenRecorder = new SpecializedScreenRecorder(gc, captureSize,
                 new Format(MediaTypeKey,MediaType.FILE, MimeTypeKey, MIME_AVI),
-                new Format(MediaTypeKey,MediaType.VIDEO,EncodingKey, ENCODING_AVI_TECHSMITH_SCREEN_CAPTURE,
-                        CompressorNameKey, ENCODING_AVI_TECHSMITH_SCREEN_CAPTURE, DepthKey, 24,
-                        FrameRateKey, Rational.valueOf(15),
-                        new Format(MediaTypeKey, MediaType.VIDEO, EncodingKey, "black"),FrameRateKey,Rational.valueOf(30),
-                        null,file, "ScreenRecorded"));
+                new Format(MediaTypeKey, MediaType.VIDEO, EncodingKey, ENCODING_AVI_TECHSMITH_SCREEN_CAPTURE,
+                        CompressorNameKey, ENCODING_AVI_TECHSMITH_SCREEN_CAPTURE, DepthKey, 24 ,FrameRateKey, Rational.valueOf(15),
+                        QualityKey,1.0f, KeyFrameIntervalKey, 15*60),
+                new Format(MediaTypeKey, MediaType.VIDEO, EncodingKey, "black", FrameRateKey, Rational.valueOf(30)),
+                null, file, "ScreenRecorded");
 
         this.screenRecorder.start();
     }
+
+    @Test
+    public void videoTest() throws IOException, AWTException {
+        VideoRecorder vr = new VideoRecorder();
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\drivers\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        vr.startRecording(System.getProperty("user.dir") + "\\video\\");
+
+
+        driver.get("https://www.google.com/");
+
+        WebElement txtSearch = driver.findElement(By.name("q"));
+        txtSearch.sendKeys("Tester");
+        txtSearch.submit();
+        System.out.println("Page title is:" + driver.getTitle());
+        driver.quit();
+        vr.stopRecording();
+
+
+    }
+
+
 }
